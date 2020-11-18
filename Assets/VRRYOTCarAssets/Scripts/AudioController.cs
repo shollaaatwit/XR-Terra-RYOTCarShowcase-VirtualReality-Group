6fdac093fe81dae80d507d3eac477437c2b6ba53
shollaaatwit <shollaa@wit.edu>
2020-11-18 14:59:42 -0500

    added audio

---
+++ Assets/VRRYOTCarAssets/Scripts/AudioController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public AudioClip[] clips;
+    public AudioClip startMessage;
+    AudioSource source;
+
+    public void PlayTutorial(int index)
+    {
+        source.clip = clips[index];
+        source.Play();
+    }
+
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+}
