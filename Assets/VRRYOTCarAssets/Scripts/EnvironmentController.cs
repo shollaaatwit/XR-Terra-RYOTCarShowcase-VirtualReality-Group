d7967e61c2df855ff36b3a51ad8dcd42426e0233
shollaaatwit <shollaa@wit.edu>
2020-11-17 21:03:51 -0500

    Implemented environment controller

---
+++ Assets/VRRYOTCarAssets/Scripts/EnvironmentController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentController : MonoBehaviour
+{
+
+    public static GameObject environment1;
+    public static GameObject environment2;
+    public static GameObject environment3;
+
+    public GameObject[] environments = new GameObject[] {environment1, environment2, environment3};
+
+    void Start()
+    {
+        environments[0].gameObject.SetActive(true);
+        environments[1].gameObject.SetActive(false);
+        environments[2].gameObject.SetActive(false);
+    }
+
+    public void ChangeEnvironment(int number)
+    {
+        switch(number)
+        {
+            case 0:
+                environments[0].gameObject.SetActive(true);
+                environments[1].gameObject.SetActive(false);
+                environments[2].gameObject.SetActive(false);
+                break;
+            case 1:
+                environments[0].gameObject.SetActive(false);
+                environments[1].gameObject.SetActive(true);
+                environments[2].gameObject.SetActive(false);
+                break;
+            case 2:
+                environments[0].gameObject.SetActive(false);
+                environments[1].gameObject.SetActive(false);
+                environments[2].gameObject.SetActive(true);
+                break;
+            default:
+                break;
+        }
+
+    }
+}
